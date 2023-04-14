6ce0f7d89c48bdf25a2e81222728eb060e98178c
alsolecki <alsolecki@gmail.com>
2023-04-14 13:26:29 -0400

    Separated table, added images and objects

---
+++ js/mytrees.js
@@ -0,0 +1,34 @@
+const trees = [ 
+    {
+        nickname: "Benny",
+        treeType: "Weeping Fig",
+        species: "Ficus benjamina",
+        age: 15,
+        outdoor: false
+    },
+    {
+        nickname: "Pele",
+        treeType: "Brazilian Raintree",
+        species: "Pithecellobium tortum",
+        age: 15,
+        outdoor: false
+    },
+    {
+        nickname: "Willem Defoliate",
+        treeType: "Willowleaf Fig",
+        species: "Ficus salicifolia",
+        age: 11,
+        outdoor: false
+    },
+    {
+        nickname: "Bitty Benny",
+        treeType: "Weeping Fig",
+        species: "Ficus benjamina",
+        age: 3,
+        outdoor: false
+    }
+];
+
+// for (let prop in trees){
+//     console.log(`${prop}: ${trees[prop]}`);
+// };
--- js/treework.js
+++ js/treework.js
@@ -2,3 +2,19 @@ $('#flash').hide().fadeIn(2500);
 $('#flash').on('click', function(){
     $('h1').css('backgroundColor', 'tomato');
 });
+
+// console.log(`Hi, my name is ${tree.nickname} and I am a ${tree.species}.`);
+
+// BELOW: inserts tree objects from mytrees.js into the page
+let html = '';
+for (let i = 0; i < trees.length; i++){
+   let tree = trees[i];
+   html += `
+   <h2>${tree.nickname}</h2>
+   <h4>${tree.treeType} | ${tree.species}</h4>
+   <p>Age: ${tree.age}</p>
+   <br>
+   `
+}
+document.querySelector('main').insertAdjacentHTML('beforeend', html);
+// ABOVE: inserts tree objects from mytrees.js into the page
